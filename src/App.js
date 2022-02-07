30f105584a88afeb8459f39239f40360780e1742
chieutrangg <chieutrangg@gmail.com>
2022-02-07 23:33:28 +0200

    added game area

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Snake from "./components/Snake"
+import Food from "./components/Food"
+
+function App() {
+  return 
+    <div className="game-area">
+        {/* <Snake snakeDots={this.state.snakeDots}/>
+        <Food dot={this.state.food}/> */}
+    </div>;
+}
+
+export default App;
