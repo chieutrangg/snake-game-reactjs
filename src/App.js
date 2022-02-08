02c8efa0891a5fa178104d068d601fdebf286f2a
chieutrangg <chieutrangg@gmail.com>
2022-02-08 23:28:15 +0200

    added snake

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Snake from "./components/Snake"
 import Food from "./components/Food"
 
+
+
 function App() {
   return (
     <div className="game-area">
--- src/components/Snake.js
+++ src/components/Snake.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
-const Snake = () => {
-  return <div></div>;
-};
-
-export default Snake;
-
+export default (props) => {
+  return (
+    <div>
+      {props.snakeDots.map((dot, i) => {
+        const style = {
+          left: `${dot[0]}%`,
+          top: `${dot[1]}%`
+        }
+        return (
+          <div className="snake-dot" key={i} style={style}></div>
+        )
+      })}
+    </div>
+  )
+}
