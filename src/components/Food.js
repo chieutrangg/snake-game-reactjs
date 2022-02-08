697540661521a001d9a995d44ee825ee880a01f0
chieutrangg <chieutrangg@gmail.com>
2022-02-09 00:00:56 +0200

    updated

--- src/components/Food.js
+++ src/components/Food.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const Food = () => {
-  return <div></div>;
-};
+export default (props) => {
 
-export default Food;
+  const style = {
+    left: `${props.dot[0]}%`,
+    top: `${props.dot[1]}%`
+  }
+
+  return (
+    <div className="snake-food" style={style}></div>
+  )
+}
