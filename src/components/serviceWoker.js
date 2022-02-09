f08d773c64229676e9f444bf1ac9b55c39f88ddf
chieutrangg <chieutrangg@gmail.com>
2022-02-09 23:57:51 +0200

    updated

--- src/components/serviceWoker.js
+++ src/components/serviceWoker.js
@@ -12,12 +12,10 @@ const isLocalhost = Boolean(
   
   export function register(config) {
     if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-      // The URL constructor is available in all browsers that support SW.
+     
       const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
       if (publicUrl.origin !== window.location.origin) {
-        // Our service worker won't work if PUBLIC_URL is on a different origin
-        // from what our page is served on. This might happen if a CDN is used to
-        // serve assets; see https://github.com/facebook/create-react-app/issues/2374
+        
         return;
       }
   
@@ -25,11 +23,10 @@ const isLocalhost = Boolean(
         const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
   
         if (isLocalhost) {
-          // This is running on localhost. Let's check if a service worker still exists or not.
+          
           checkValidServiceWorker(swUrl, config);
   
-          // Add some additional logging to localhost, pointing developers to the
-          // service worker/PWA documentation.
+        
           navigator.serviceWorker.ready.then(() => {
             console.log(
               'This web app is being served cache-first by a service ' +
@@ -37,11 +34,55 @@ const isLocalhost = Boolean(
             );
           });
         } else {
-          // Is not localhost. Just register service worker
+        
           registerValidSW(swUrl, config);
         }
       });
     }
+  }
+  
+  function registerValidSW(swUrl, config) {
+    navigator.serviceWorker
+      .register(swUrl)
+      .then(registration => {
+        registration.onupdatefound = () => {
+          const installingWorker = registration.installing;
+          if (installingWorker == null) {
+            return;
+          }
+          installingWorker.onstatechange = () => {
+            if (installingWorker.state === 'installed') {
+              if (navigator.serviceWorker.controller) {
+                // At this point, the updated precached content has been fetched,
+                // but the previous service worker will still serve the older
+                // content until all client tabs are closed.
+                console.log(
+                  'New content is available and will be used when all ' +
+                    'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
+                );
+  
+                // Execute callback
+                if (config && config.onUpdate) {
+                  config.onUpdate(registration);
+                }
+              } else {
+                // At this point, everything has been precached.
+                // It's the perfect time to display a
+                // "Content is cached for offline use." message.
+                console.log('Content is cached for offline use.');
+  
+                // Execute callback
+                if (config && config.onSuccess) {
+                  config.onSuccess(registration);
+                }
+              }
+            }
+          };
+        };
+      })
+      .catch(error => {
+        console.error('Error during service worker registration:', error);
+      });
   }
   
   
